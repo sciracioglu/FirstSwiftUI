341614fe9006b8a6ac9ba7f066b39face455b2fc
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-10-30 04:42:10 +0300

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Serkan Ciracioglu on 30.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
